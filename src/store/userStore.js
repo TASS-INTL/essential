6038566fa831e22d8563585b658b5cfbc5ed31ff
Camilo Gonzales <camilo@MacBook-Air-de-Usuario.local>
2024-01-09 16:50:17 -0500

    se eliminaron los campos que no se utilizan en el estado global del usuario

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -4,18 +4,16 @@ import { persist } from 'zustand/middleware'
 export const userStore = create(
 	persist(
 		(set) => ({
-			token: false,
 			userData: {
 				uid: '',
 				name: '',
 				email: '',
-				token: '',
 				userName: '',
 				logged: false,
 				checking: true,
-				tokenRegister: null
+				tokenRegister: null,
+				tokenSesion: null
 			},
-			setToken: (value) => set(() => ({ token: value })),
 			setUserData: (value) => set(() => ({ userData: value }))
 		}),
 		{ name: 'user-storage' }
