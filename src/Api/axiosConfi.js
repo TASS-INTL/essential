e0de6ee7e40c524441e210d53151c3d2327eb027
Camilo Gonzales <camilo@MacBook-Air-de-Usuario.local>
2024-01-22 08:56:20 -0500

    se configuro la nueva forma de hacer peticiones con la intancia de axiox

--- src/Api/axiosConfi.js
+++ src/Api/axiosConfi.js
@@ -1,8 +1,13 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://ec2-15-229-119-48.sa-east-1.compute.amazonaws.com:8000/api/v1/'
+const BASE_URL = 'http://ec2-15-228-13-62.sa-east-1.compute.amazonaws.com:8000/api/v1/'
 
 export const AXIOSINSTANCE = axios.create({
 	baseURL: BASE_URL,
-	headers: { 'Content-Type': 'application/json' }
+	headers: { 'Content-Type': 'application/json', 'x-access-token': localStorage.getItem('token') }
 })
+
+// export const AXIOSINSTANCEFILE = axios.create({
+// 	baseURL: BASE_URL,
+// 	headers: { 'x-access-token': 'multipart/form-data' }
+// })
