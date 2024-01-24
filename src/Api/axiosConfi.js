22effef5240e48e4f5ba8cb4c50940bf85ed706c
Camilo Gonzales <camilo@MacBook-Air-de-Usuario.local>
2024-01-24 16:56:02 -0500

    fix: se soluciono un bug a la hora de hacer la peticion por primera vez, no enviaba el token de sesion para hacer la peticion

--- src/Api/axiosConfi.js
+++ src/Api/axiosConfi.js
@@ -3,11 +3,37 @@ import axios from 'axios'
 const BASE_URL = 'http://ec2-15-228-13-62.sa-east-1.compute.amazonaws.com:8000/api/v1/'
 
 export const AXIOSINSTANCE = axios.create({
-	baseURL: BASE_URL,
-	headers: { 'Content-Type': 'application/json', 'x-access-token': localStorage.getItem('token') }
+	baseURL: BASE_URL
 })
 
-// export const AXIOSINSTANCEFILE = axios.create({
-// 	baseURL: BASE_URL,
-// 	headers: { 'x-access-token': 'multipart/form-data' }
-// })
+// Request interceptor for API calls
+AXIOSINSTANCE.interceptors.request.use(
+	async (config) => {
+		const token = await localStorage.getItem('token')
+		config.headers = {
+			Accept: 'application/json',
+			'x-access-token': `${token}`,
+			'Content-Type': 'application/json'
+		}
+		return config
+	},
+	(error) => {
+		Promise.reject(error)
+	}
+)
+
+// AXIOSINSTANCE.interceptors.response.use(
+// 	(response) => {
+// 		return response
+// 	},
+// 	async function (error) {
+// 		const originalRequest = error.config
+// 		if (error.response.status === 403 && !originalRequest._retry) {
+// 			originalRequest._retry = true
+// 			const access_token = await refreshAccessToken()
+// 			axios.defaults.headers.common['Authorization'] = 'Bearer ' + access_token
+// 			return axiosApiInstance(originalRequest)
+// 		}
+// 		return Promise.reject(error)
+// 	}
+// )
